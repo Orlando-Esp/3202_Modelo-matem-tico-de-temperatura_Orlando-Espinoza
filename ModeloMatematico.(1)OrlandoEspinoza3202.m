d6eb0745ca21a820d8ccda8698d9a8c9ada0e14b
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-04-15 23:20:40 -0500

    Update ModeloMatematico.(1)OrlandoEspinoza3202.m

--- ModeloMatematico.(1)OrlandoEspinoza3202.m
+++ ModeloMatematico.(1)OrlandoEspinoza3202.m
@@ -1,5 +1,5 @@
 % Octave Script
-% Title			:ModeloMatematicode(1)
+% Title			:ModeloMatematico(1)
 % Description		:Script para graficar una funcion del cambio de temperatura en grados °F y °C de la forma f(x)=ax+b   
 % Author		:Orlando Espinoza Valentin (Orlando-Esp) espinozaorlando978@gmail.com
 % Date			:20210415
